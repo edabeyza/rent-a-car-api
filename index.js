af78e32768febae9c6220bbfdd1496714f408561
edabeyza <edabeyzaozeren@gmail.com>
2024-08-04 01:21:13 +0300

    dbConnection done

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 /* -------------------------------------------------------------------------- */
 /*                           RENT A CAR API PROJECT                           */
 /* -------------------------------------------------------------------------- */
+
+/* -------------------------------- requires -------------------------------- */
+const express = require('express')
+const app = express()
+
+require('express-async-errors')
+
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+
+/* ------------------------------ dbConnection ------------------------------ */
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+/* ------------------------------- run server ------------------------------- */
+app.listen(PORT, () => console.log('Running on http://127.0.0.1:' + PORT))
--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -2,3 +2,17 @@
 /* -------------------------------------------------------------------------- */
 /*                           RENT A CAR API PROJECT                           */
 /* -------------------------------------------------------------------------- */
+
+/* --------------------------- mongodb connection --------------------------- */
+const mongoose = require('mongoose')
+
+const dbConnection = function() {
+    mongoose.connect(process.env.MONGODB)
+        .then(() => console.log('** Db Is Connected Succesfully **'))
+        .catch((err) => console.log('-- Db Is Not Connected --', err))
+}
+
+module.exports={
+    mongoose,
+    dbConnection
+}
